ddd2713f39a5a2056c0d9f943090e849eb794d99
behrooz pahlavan <pahlavan111@gmail.com>
2020-07-12 09:26:31 +0430

    create data package and Movie class

---
+++ app/src/main/java/com/bp/batman/data/Movie.java
@@ -0,0 +1,11 @@
+package com.bp.batman.data;
+
+public class Movie {
+
+    private int id;
+    private String Title;
+    private String Year;
+    private String imdbId;
+    private String Type;
+    private String Poster;
+}
