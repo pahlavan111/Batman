2ed97519464fb45bb5a6d5594d6330f51d24144c
behrooz pahlavan <pahlavan111@gmail.com>
2020-07-13 16:52:52 +0430

    update Movie Model and create Rating Model

--- app/src/main/java/com/bp/batman/data/Movie.java
+++ app/src/main/java/com/bp/batman/data/Movie.java
@@ -1,5 +1,7 @@
 package com.bp.batman.data;
 
+import java.util.List;
+
 public class Movie {
 
     public Movie(String title, String year, String imdbID, String type, String poster) {
@@ -16,6 +18,26 @@ public class Movie {
     private String imdbID;
     private String Type;
     private String Poster;
+    private String Rated;
+    private String Released;
+    private String Runtime;
+    private String Genre;
+    private String Director;
+    private String Writer;
+    private String Actors;
+    private String Plot;
+    private String Language;
+    private String Country;
+    private String Awards;
+    private List<Rating> Ratings;
+    private String Metascore;
+    private String imdbRating;
+    private String imdbVotes;
+    private String DVD;
+    private String BoxOffice;
+    private String Production;
+    private String Website;
+    private String Response;
 
     public int getId() {
         return id;
@@ -63,5 +85,165 @@ public class Movie {
 
     public void setPoster(String poster) {
         Poster = poster;
+    }
+
+    public String getRated() {
+        return Rated;
+    }
+
+    public void setRated(String rated) {
+        Rated = rated;
+    }
+
+    public String getReleased() {
+        return Released;
+    }
+
+    public void setReleased(String released) {
+        Released = released;
+    }
+
+    public String getRuntime() {
+        return Runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        Runtime = runtime;
+    }
+
+    public String getGenre() {
+        return Genre;
+    }
+
+    public void setGenre(String genre) {
+        Genre = genre;
+    }
+
+    public String getDirector() {
+        return Director;
+    }
+
+    public void setDirector(String director) {
+        Director = director;
+    }
+
+    public String getWriter() {
+        return Writer;
+    }
+
+    public void setWriter(String writer) {
+        Writer = writer;
+    }
+
+    public String getActors() {
+        return Actors;
+    }
+
+    public void setActors(String actors) {
+        Actors = actors;
+    }
+
+    public String getPlot() {
+        return Plot;
+    }
+
+    public void setPlot(String plot) {
+        Plot = plot;
+    }
+
+    public String getLanguage() {
+        return Language;
+    }
+
+    public void setLanguage(String language) {
+        Language = language;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String country) {
+        Country = country;
+    }
+
+    public String getAwards() {
+        return Awards;
+    }
+
+    public void setAwards(String awards) {
+        Awards = awards;
+    }
+
+    public List<Rating> getRatings() {
+        return Ratings;
+    }
+
+    public void setRatings(List<Rating> ratings) {
+        Ratings = ratings;
+    }
+
+    public String getMetascore() {
+        return Metascore;
+    }
+
+    public void setMetascore(String metascore) {
+        Metascore = metascore;
+    }
+
+    public String getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(String imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+
+    public String getImdbVotes() {
+        return imdbVotes;
+    }
+
+    public void setImdbVotes(String imdbVotes) {
+        this.imdbVotes = imdbVotes;
+    }
+
+    public String getDVD() {
+        return DVD;
+    }
+
+    public void setDVD(String DVD) {
+        this.DVD = DVD;
+    }
+
+    public String getBoxOffice() {
+        return BoxOffice;
+    }
+
+    public void setBoxOffice(String boxOffice) {
+        BoxOffice = boxOffice;
+    }
+
+    public String getProduction() {
+        return Production;
+    }
+
+    public void setProduction(String production) {
+        Production = production;
+    }
+
+    public String getWebsite() {
+        return Website;
+    }
+
+    public void setWebsite(String website) {
+        Website = website;
+    }
+
+    public String getResponse() {
+        return Response;
+    }
+
+    public void setResponse(String response) {
+        Response = response;
     }
 }
---
+++ app/src/main/java/com/bp/batman/data/Rating.java
@@ -0,0 +1,24 @@
+package com.bp.batman.data;
+
+public class Rating {
+
+    String Source;
+    String Value;
+
+    public String getSource() {
+        return Source;
+    }
+
+    public void setSource(String source) {
+        Source = source;
+    }
+
+    public String getValue() {
+        return Value;
+    }
+
+    public void setValue(String value) {
+        Value = value;
+    }
+
+}
