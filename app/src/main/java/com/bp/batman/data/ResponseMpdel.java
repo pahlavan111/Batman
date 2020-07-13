1b1b2a930574f977489d385088bc44488e06eb0f
behrooz pahlavan <pahlavan111@gmail.com>
2020-07-13 08:02:53 +0430

    Added Response Model to data layer

---
+++ app/src/main/java/com/bp/batman/data/ResponseMpdel.java
@@ -0,0 +1,34 @@
+package com.bp.batman.data;
+
+import java.util.List;
+
+public class ResponseMpdel {
+
+    List<Movie> Search;
+    Integer totalResults;
+    Boolean Response;
+
+    public List<Movie> getSearch() {
+        return Search;
+    }
+
+    public void setSearch(List<Movie> search) {
+        Search = search;
+    }
+
+    public Integer getTotalResults() {
+        return totalResults;
+    }
+
+    public void setTotalResults(Integer totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public Boolean getResponse() {
+        return Response;
+    }
+
+    public void setResponse(Boolean response) {
+        Response = response;
+    }
+}
